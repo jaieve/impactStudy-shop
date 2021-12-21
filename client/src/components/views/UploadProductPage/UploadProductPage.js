5d34cb24fee6c36c8fdfbbb8dc2485422c9961a4
jaieve-ps <jaieve1469@polestarhc.com>
2021-12-21 23:44:20 +0900

    init commit

---
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//vscode에서는 rfc 이지만, webstorm에서는 rsf이다.
+// rcc 는 똑같이 rcc
+
+
+function UploadProductPage() {
+    return (
+        <div>
+            안녕하세요!
+        </div>
+    );
+}
+
+export default UploadProductPage;
