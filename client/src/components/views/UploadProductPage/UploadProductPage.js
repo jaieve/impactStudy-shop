67276c5183b90043d3d66e01f9b6670f77c4ecdf
jaieve-ps <yoori0303@naver.com>
2021-12-22 22:53:06 +0900

    select option 처리

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -2,10 +2,19 @@ import React, {useState} from 'react';
 import {Typography, Button, Form, Input} from "antd";
 //vscode에서는 rfc 이지만, webstorm에서는 rsf이다.
 // rcc 는 똑같이 rcc
-
-
 const {Title} = Typography;
-const {TextArea} = Input
+const {TextArea} = Input;
+
+const Continents = [
+    {key : 1, value: 'Africa'},
+    {key : 2, value: 'Europe'},
+    {key : 3, value: 'Asia'},
+    {key : 4, value: 'North America'},
+    {key : 5, value: 'South America'},
+    {key : 6, value: 'Australia'},
+    {key : 7, value: 'Antarctica'},
+]
+
 function UploadProductPage() {
     const [Title, setTitle ] = useState("")
     const [Description, setDescription] = useState("")
@@ -21,6 +30,9 @@ function UploadProductPage() {
     }
     const handleNumberChange = (e) => {
         setPrice(e.currentTarget.value);
+    }
+    const handleContinentChange= (e) => {
+        setContinent(e.currentTarget.value);
     }
     return (
         <div style={{ maxWidth: '700px', margin: '2rem auto'}}>
@@ -43,8 +55,10 @@ function UploadProductPage() {
                 <Input type="number" onChange={handleNumberChange} value={Price}/>
                 <br/>
                 <br/>
-                <select>
-                    <option></option>
+                <select onChange={handleContinentChange} value={Continent}>
+                    {Continents.map( item => (
+                        <option key={item.key} value={item.key}>{item.value}</option>
+                        ))}
                 </select>
                 <br/>
                 <br/>
