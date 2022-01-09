8bb3e2b806d30137e9f821eb5f5443e5e94aa004
jaieve-ps <yoori0303@naver.com>
2022-01-09 11:53:03 +0900

    라디오박스 필터 추가

--- server/models/Product.js
+++ server/models/Product.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const productSchema = mongoose.Schema({
     writer: {
         type: Schema.Types.ObjectId,
-        ref: 'User'
+        ref: 'Product'
     },
     title: {
         type: String,
