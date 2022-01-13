d9e8d74607a9ac96feb820441cac0bded37c8be1
jaieve-ps <jaieve1469@polestarhc.com>
2022-01-13 20:31:05 +0900

    Product Info 컴포넌트 만들기

---
+++ client/src/components/views/DetailProductPage/Sections/ProductInfo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ProductInfo(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default ProductInfo;
--- server/routes/product.js
+++ server/routes/product.js
@@ -90,7 +90,7 @@ router.post('/products', (req, res) => {
 router.get('/products_by_id', (req, res) => {
     let type = req.query.type
     let productId = req.query.id
-    // productId를 이용해 DB에서 productID와 같은 상품의 정ㅈ보를 가져온다.
+    // productId를 이용해 DB에서 productID와 같은 상품의 정보를 가져온다.
     Product.find({_id : productId})
         .populate('writer')
         .exec((err, product) => {
