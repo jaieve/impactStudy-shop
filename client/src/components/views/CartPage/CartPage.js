466601f6c675ca11281aa1aabed555dbeddea5c9
jaieve-ps <yoori0303@naver.com>
2022-02-05 13:42:36 +0900

    Paypal 코드 추가

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -2,10 +2,12 @@ import React, {useEffect, useState} from 'react';
 import {useDispatch} from "react-redux";
 import {getCartItems, removeCartItem} from '../../../_actions/user_actions';
 import UserCardBlock from "./Sections/UserCardBlock";
+import { Empty } from 'antd';
 
 function CartPage(props) {
     const dispatch = useDispatch();
     const [Total, setTotal] = useState(0);
+    const [ShowTotal, setShowTotal] =useState(false);
 
     useEffect( () => {
         let cartItems = [];
@@ -27,11 +29,14 @@ function CartPage(props) {
             total += parseInt(item.price,10) * item.quantity
         })
         setTotal(total);
+        setShowTotal(true);
     }
     let removeFromCart = (productId) => {
         dispatch(removeCartItem(productId))
             .then(response => {
-
+                if (response.payload.productInfo.length <= 0 ) {
+                    setShowTotal(false);
+                }
             })
     }
 
@@ -41,9 +46,17 @@ function CartPage(props) {
             <div>
                 <UserCardBlock products={props.user.cartDetail} removeItem={removeFromCart}/>
             </div>
-            <div style={{ marginTop: '3rem' }}>
-                <h2>Total Amount: ${Total}</h2>
-            </div>
+            {ShowTotal ?
+                <div style={{ marginTop: '3rem' }}>
+                    <h2>Total Amount: ${Total}</h2>
+                </div>
+            :
+                <>
+                    <br />
+                    <Empty description={false} />
+                </>
+            }
+
         </div>
     );
 }
